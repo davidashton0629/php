0dba7d99c69112c582b404cede2cfda1721efe0d
davidashton0629 <77017046+davidashton0629@users.noreply.github.com>
2021-01-08 09:51:37 -0500

    Create breadcrumb.php

---
+++ breadcrumb.php
@@ -0,0 +1,44 @@
+function breadcrumbs($options = array())
+{
+	$o = array_merge(array(
+		'qLink'=>true
+	), $options);
+	
+	extract($o);
+	
+	$crumbs = explode("/",$_SERVER["REQUEST_URI"]);
+	$link = $data = "";
+	
+	if($qLink) {
+		$data .= "<span id='identifier'>Quick Links</span> - ";
+	}
+	
+	
+	$total = count($crumbs); 
+	if(end($crumbs) == "" || end($crumbs) == "/") {
+		array_pop($crumbs);
+	}
+	
+	foreach($crumbs as $crumb){
+		$item = strtolower(str_replace(array(".php","_","/"),array(""," ",""),$crumb));
+		if($item !== '' && $item !== '/') {
+			if($crumb !== $crumbs[count($crumbs) -1]) {
+				$link .= $item . '/';
+				if($item !== "view" && $item !== 'category') {
+					$data .= "<a href='/$link'>$item</a> / ";
+				}
+			} else {
+				$link .= $item;
+				if($item !== "view" && $item !== 'category') {
+					$data .= "<a href='/$link'>$item</a>";
+				}
+			}
+		} else {
+			
+		}
+	}
+	
+	return $data;
+}
+
+echo breadcrumbs();
